e9f761493d3eb6bff4b1a8480b542ea148e82617
Vladislava <stdemidovez@gmail.com>
2024-11-14 18:14:58 +0300

    chore: add eslint-plugin-react

--- eslint.config.js
+++ eslint.config.js
@@ -4,10 +4,13 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-plugin-prettier'
+import react from 'eslint-plugin-react'
+
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
+    settings: { react: { version: '18.3' } },
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
@@ -22,6 +25,7 @@ export default tseslint.config(
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       prettier, // Add Prettier plugin
+      react
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -30,6 +34,8 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       'prettier/prettier': ['error'],
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
     },
   },
 )
