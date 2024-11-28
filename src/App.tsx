7f1325e81f2e23db3cd38b44bdee0b9018df59be
Vladislava <stdemidovez@gmail.com>
2024-11-29 00:35:11 +0300

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from './components/containers/header/header.tsx';
 import { MainPage } from './pages/main-page.tsx';
+import { Footer } from './components/containers/footer/footer.tsx';
 
 function App() {
   return (
     <>
       <Header />
       <MainPage />
+      <Footer />
     </>
   );
 }
---
+++ src/components/containers/footer/footer.tsx
@@ -0,0 +1,9 @@
+import styles from './footer.module.css';
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <span>Made by HappyHouse — Copyright 2024</span>
+    </footer>
+  );
+};
